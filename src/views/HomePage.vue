70c55483c44b4ae6221e0f39708096a3b1dfd7e3
AleRuizdV <al.ruizdevinaspre@duocuc.cl>
2024-09-05 10:48:07 -0400

    Cards_separation(Update)

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -12,7 +12,7 @@
           <ion-title size="large">Blank</ion-title>
         </ion-toolbar>
       </ion-header>
-
+      <!--
       <div id="container">
         <strong>Ready to create an app?</strong>
         <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
@@ -23,25 +23,44 @@
 
         <p>Texto "p"</p>
       </div>
+      -->
+      
+      <ion-card>
+        <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/card-media.png" />
+        <ion-card-header>
+          <ion-card-title>Card Title</ion-card-title>
+          <ion-card-subtitle>Card Subtitle</ion-card-subtitle>
+        </ion-card-header>
+
+        <ion-card-content>
+          Here's a small text description for the card content. Nothing more, nothing less.
+        </ion-card-content>
+      </ion-card>
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
 import { 
+  // Componente Base
   IonContent, 
   IonHeader, 
   IonPage, 
   IonTitle, 
   IonToolbar,
-  IonChip 
+  IonChip,
+  // Componentes Cartas
+  IonCard, 
+  IonCardContent, 
+  IonCardHeader, 
+  IonCardSubtitle, 
+  IonCardTitle
   } from '@ionic/vue';
 </script>
 
 <style scoped>
 #container {
   text-align: center;
-  
   position: absolute;
   left: 0;
   right: 0;
@@ -57,9 +76,7 @@ import {
 #container p {
   font-size: 16px;
   line-height: 22px;
-  
   color: #8c8c8c;
-  
   margin: 0;
 }
 
